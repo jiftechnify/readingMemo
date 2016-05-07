a34a1abb891da6ed9c777a9b7acfae8fb32ead0b
jiftech <jiftech.stlfy@gmail.com>
2016-05-07 21:11:20 +0900

    c2

---
+++ 7lang7week/example/acts_as_csv_module.rb
@@ -0,0 +1,50 @@
+module ActsAsCsv
+  # base: これをincludeするクラス(ここではRubyCsv)
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def acts_as_csv
+      include InstanceMethods
+    end
+  end
+
+  module InstanceMethods
+    def read
+      @csv_contents = []
+      filename = self.class.to_s.downcase + '.txt'
+      file = File.new(filename)
+      @headers = file.gets.chomp.split(',')
+
+      file.each do |row|
+        @csv_contents << row.chomp.split(",")
+      end
+    end
+
+    attr_accessor :headers, :csv_contents
+
+    def initialize
+      read
+    end
+  end
+end
+
+class RubyCsv # no inheritance! You can mix it in
+  include ActsAsCsv
+  acts_as_csv
+end
+
+m = RubyCsv.new
+puts m.headers.inspect
+puts m.csv_contents.inspect
+
+# セルフスタディ3日目
+# 以下のようなAPIが使えるようにこのクラスを変更
+# rubycsv.txt:
+# one, two
+# lions, tigers
+#
+# csv = RubyCsv.new
+# csv.each {|row| puts row.one}
+# => lions
