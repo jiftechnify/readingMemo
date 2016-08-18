d286324020da6385a6213944f6141f66b3636ff6
jiftech <jiftech.stlfy@gmail.com>
2016-08-18 18:41:21 +0900

    prolog

--- 7lang7week/example/prolog/eight_queens.pl
+++ 7lang7week/example/prolog/eight_queens.pl
@@ -1,32 +1,40 @@
+/* クイーンの座標が妥当か? */
 valid_queen((_, Col)) :-
   Range = [1,2,3,4,5,6,7,8],
   member(Col, Range).
 
+/* 盤面が妥当: 全てのクイーンの座標が妥当 */
 valid_board([]).
 valid_board([Head|Tail]) :- valid_queen(Head), valid_board(Tail).
 
+/* 全てのクイーンの列の座標のリスト */
 cols([], []).
 cols([(_, Col)|QueensTail], [Col|ColsTail]) :-
   cols(QueensTail, ColsTail).
 
+/* クイーンがどの斜め線上にあるかのリスト1 */
 diags1([] , []).
 diags1([(Row, Col)|QueensTail], [Diagonal|DiagonalsTail]) :-
   Diagonal is Col - Row,
   diags1(QueensTail, DiagonalsTail).
 
+/* クイーンがどの斜め線上にあるかのリスト2 */
 diags2([] , []).
 diags2([(Row, Col)|QueensTail], [Diagonal|DiagonalsTail]) :-
   Diagonal is Col + Row,
   diags2(QueensTail, DiagonalsTail).
 
 eight_queens(Board) :-
+  /* この段階でクイーンは全て異なる行にあると定まる */
   Board = [(1, _), (2, _), (3, _), (4, _), (5, _), (6, _), (7, _), (8, _)],
   valid_board(Board),
 
+  /* クイーンの列座標のリスト、およびどの斜め線上にあるかのリストを構成 */
   cols(Board, Cols),
   diags1(Board, Diags1),
   diags2(Board, Diags2),
 
+  /* 同じ列と斜め方向に複数のクイーンがないか? */
   fd_all_different(Cols),
   fd_all_different(Diags1),
   fd_all_different(Diags2).
--- 7lang7week/example/prolog/sudoku.pl
+++ 7lang7week/example/prolog/sudoku.pl
@@ -1,31 +1,39 @@
+/* 行･列･ブロックが妥当(全ての数字が異なる)かを判定する */
 valid([]).
 valid([Head|Tail]) :-
   fd_all_different(Head),
   valid(Tail).
 
+/* Puzzleに与えられた数独を解きSolutionにバインドする */
 sudoku(Puzzle, Solution) :-
+  /* 入力された問題とその解はユニファイできなければならない */
   Solution = Puzzle,
+  /* リストの要素とマス目の名前の対応付け。またリストの全要素は1から4の数字でなければならない */
   Puzzle = [S11, S12, S13, S14,
             S21, S22, S23, S24,
             S31, S32, S33, S34,
             S41, S42, S43, S44],
-  fd_domain(Puzzle, 1, 4),
+  fd_domain(Solution, 1, 4),
 
+  /* 行の定義(ex.1行目は11,12,13,14のマス目からなる) */
   Row1 = [S11, S12, S13, S14],
   Row2 = [S21, S22, S23, S24],
   Row3 = [S31, S32, S33, S34],
   Row4 = [S41, S42, S43, S44],
 
+  /* 列の定義 */
   Col1 = [S11, S21, S31, S41],
   Col2 = [S12, S22, S32, S42],
   Col3 = [S13, S23, S33, S43],
   Col4 = [S14, S24, S34, S44],
 
+  /* ブロックの定義 */
   Square1 = [S11, S12, S21, S22],
   Square2 = [S13, S14, S23, S24],
   Square3 = [S31, S32, S41, S42],
   Square4 = [S33, S34, S43, S44],
 
+  /* 全ての行･列･ブロックが妥当でなければならない */
   valid([Row1, Row2, Row3, Row4,
          Col1, Col2, Col3, Col4,
          Square1, Square2, Square3, Square4]).
