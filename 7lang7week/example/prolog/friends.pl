adcc132094e57ea2fb4e2f183fc62cea181a1b6d
jiftech <jiftech.stlfy@gmail.com>
2016-08-18 14:36:47 +0900

    prolog

--- 7lang7week/example/prolog/friends.pl
+++ 7lang7week/example/prolog/friends.pl
@@ -1,5 +1,7 @@
+/* likes(a, b): "a likes b" */
 likes(wallace, cheese).
 likes(grommit, cheese).
 likes(wendolene, sheep).
 
+/* friend(X, Y): "If X is not equal to Y, and both X and Y like same things, then X and Y are friends." */
 friend(X, Y) :- \+(X = Y), likes(X, Z), likes(Y, Z).
