00b4a56732bb2590622f1068d3df1c92d1c2aaf9
jiftechnify <jiftech.stlfy@gmail.com>
2018-02-08 00:16:36 +0900

    refactor readmemo.js

--- readmemo.js
+++ readmemo.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const rs = require('readline-sync');
 
 const exists = (path) => {
@@ -12,41 +13,38 @@ const exists = (path) => {
 }
 
 const loop = () => {
-  let dirname = rs.question('プロジェクト名を入力:');
-  if (exists(`./${dirname}`)) {
+  let projectDir = rs.question('プロジェクト名を入力:');
+  if (exists(path.join(__dirname, projectDir))) {
     console.log('同じ名前のプロジェクトが存在します。別の名前を指定してください: ');
     loop();
   }
-  return dirname;
+  return projectDir;
+}
+
+const printAll = (path, contents) => {
+  if(typeof contents === 'string') {
+    contents = [contents];
+  }
+  const joined = contents.join('\n') + '\n';
+  if(!exists(path)) {
+    fs.writeFileSync(path, joined);
+  }
+  else {
+    fs.appendFileSync(path, joined);
+  }
 }
 
 console.log('新規読書メモプロジェクトを構成します');
-let dirname = loop();
+let projectDir = loop();
 let book = rs.question('本のタイトルを入力:');
 let chapters = rs.question('章の系列をコンマ区切りで入力(例: 1,2-3,4-6):');
 
-const index = `./${dirname}/index.md`;
-const md = (chap) => {
-  `./${dirname}/$c{chap}.md`;
-}
-const println = (path, contents) => {
-  if(typeof contents !== 'object') {
-    contents = [contents];
-  }
-
-  if(!exists(path)) {
-    let c = contents.shift();
-    fs.writeFileSync(path, `${c}\n`);
-  }
-  contents.forEach((c) => {
-    fs.appendFileSync(path, `${c}\n`);
-  });
-}
+const index = path.join(__dirname, projectDir, 'index.md');
 
 try {
-  fs.mkdir(`./${dirname}`);
-  println('./readme.md', `+ [${book}](${dirname}/index.md)`);
-  println(index, [
+  fs.mkdir(path.join(__dirname, projectDir));
+  printAll(path.join(__dirname, '/readme.md'), `+ [${book}](${projectDir}/index.md)`);
+  printAll(index, [
     `# ${book} メモ`,
     `[戻る](../../../tree/master)`,
     ``,
@@ -55,40 +53,40 @@ try {
 
   let chapSplit = chapters.split(',');
   chapSplit.forEach((chap, idx) => {
-    let mdchap = `./${dirname}/${chap}.md`;
+    let mdchap = path.join(__dirname, projectDir, `${chap}.md`);
     let title = chap.replace('-', '～');
 
-    println(index, `+ [${title}章](${chap}.md)`);
-    println(mdchap, [
+    printAll(index, `+ [${title}章](${chap}.md)`);
+    printAll(mdchap, [
       `# ${book} 第${title}章`,
       `[全体目次へ戻る](index.md)`,
       ``,
     ]);
 
     if(chap.indexOf('-') < 0) {
-      println(mdchap, `## ${chap}章`);
+      printAll(mdchap, `## ${chap}章`);
     }
     else {
       let [start, end] = chap.split('-').map((v) => parseInt(v));
       for(let i = start; i <= end; i++) {
-        println(mdchap, `## ${i}章`);
+        printAll(mdchap, `## ${i}章`);
       }
     }
-    println(mdchap, [
+    printAll(mdchap, [
       ``,
       `***`,
       ``
     ]);
     if(idx > 0) {
-      println(mdchap, `[前へ](c${chapSplit[idx - 1]}.md) /`);
+      printAll(mdchap, `[前へ](c${chapSplit[idx - 1]}.md) /`);
     }
-    println(mdchap, `[全体目次へ戻る](index.md) /`);
+    printAll(mdchap, `[全体目次へ戻る](index.md) /`);
     if(idx < chapSplit.length - 1) {
-      println(mdchap, `[次へ](c${chapSplit[idx + 1]}.md)`);
+      printAll(mdchap, `[次へ](c${chapSplit[idx + 1]}.md)`);
     }
   });
 
-  println(index, [
+  printAll(index, [
     ``,
     `## 情報`,
     ``,
